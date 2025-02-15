d86b83bb3b04937ff83c4b09525d86883cdf3dec
quanptit <quanptit410@gmail.com>
2025-02-15 22:55:51 +0700

    uu

--- lib/src/widgets/alphanumeric_keyboard.dart
+++ lib/src/widgets/alphanumeric_keyboard.dart
@@ -6,7 +6,7 @@ import '../utils/key_rows.dart';
 class AlphanumericKeyboard extends StatefulWidget {
   const AlphanumericKeyboard({
     required this.controller,
-    this.height = 260,
+    this.height,
     this.backgroundColor = const Color(0xff0a0a0a),
     this.actionKeyColor = const Color(0xff171717),
     this.alphanumericKeyColor = const Color(0xff2d2d2d),
@@ -28,7 +28,7 @@ class AlphanumericKeyboard extends StatefulWidget {
   });
 
   /// The height of the keyboard
-  final double height;
+  final double? height;
 
   /// The controller for the text field
   final TextEditingController controller;
@@ -90,54 +90,62 @@ class AlphanumericKeyboard extends StatefulWidget {
 
 class _AlphanumericKeyboardState extends State<AlphanumericKeyboard> {
   Capitalization capitalization = Capitalization.onlyFirstLetter;
+  static const double marginKey = 2.5;
 
   KeyBoardType keyboardType = KeyBoardType.alphanumeric;
 
   // Renders the keyboard rows
   Widget keyboardRow(List<String> keys) {
+    List<Widget> childrens = keys
+        .map(
+          (e) => e == SpecialKey.space.name
+              ? actionKey(SpecialKey.space, 4)
+              : e.length > 1
+                  ? actionKey(SpecialKey.values.firstWhere((element) => element.name == e), 2)
+                  : keys[0] == '1'
+                      ? numberKey(e, 1)
+                      : alphabetKey(e, 1),
+        )
+        .toList();
+    //TODO check 9-10-6 to add space
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: keys
-          .map(
-            (e) => e == SpecialKey.space.name
-                ? Expanded(child: actionKey(SpecialKey.space))
-                : e.length > 1
-                    ? actionKey(SpecialKey.values
-                        .firstWhere((element) => element.name == e))
-                    : keys[0] == '1'
-                        ? numberKey(e)
-                        : alphabetKey(e),
-          )
-          .toList(),
+      children: childrens,
     );
   }
 
   // Renders the number keys
-  Widget numberKey(String kKey) {
-    return InkWell(
-      onTap: () {
-        widget.controller.text += kKey;
-      },
-      borderRadius: BorderRadius.circular(widget.keyBorderRadius),
-      child: Container(
-        decoration: BoxDecoration(
-          color: widget.alphanumericKeyColor,
+  Widget numberKey(String kKey, int flex) {
+    return Flexible(
+      flex: flex,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            widget.controller.text += kKey;
+          },
           borderRadius: BorderRadius.circular(widget.keyBorderRadius),
-        ),
-        constraints: const BoxConstraints(
-          minWidth: 30,
-          minHeight: 35,
-        ),
-        margin: const EdgeInsets.all(3.5),
-        child: Center(
-          child: Text(
-            kKey,
-            style: widget.numericKeyTextStyle ??
-                const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: widget.alphanumericKeyColor,
+              borderRadius: BorderRadius.circular(widget.keyBorderRadius),
+            ),
+            constraints: const BoxConstraints(
+              // minWidth: 30,
+              minHeight: 35,
+            ),
+            margin: const EdgeInsets.all(marginKey),
+            child: Center(
+              child: Text(
+                kKey,
+                style: widget.numericKeyTextStyle ??
+                    const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
           ),
         ),
       ),
@@ -145,40 +153,42 @@ class _AlphanumericKeyboardState extends State<AlphanumericKeyboard> {
   }
 
   // Renders the alphabet keys
-  Widget alphabetKey(String kKey) {
-    return InkWell(
-      onTap: () {
-        widget.controller.text += capitalization == Capitalization.lowerCase
-            ? kKey.toLowerCase()
-            : kKey;
-        if (capitalization == Capitalization.onlyFirstLetter) {
-          setState(() {
-            capitalization = Capitalization.lowerCase;
-          });
-        }
-      },
-      borderRadius: BorderRadius.circular(widget.keyBorderRadius),
-      child: Container(
-        decoration: BoxDecoration(
-          color: widget.alphanumericKeyColor,
+  Widget alphabetKey(String kKey, int flex) {
+    return Flexible(
+      flex: flex,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            widget.controller.text += capitalization == Capitalization.lowerCase ? kKey.toLowerCase() : kKey;
+            if (capitalization == Capitalization.onlyFirstLetter) {
+              setState(() {
+                capitalization = Capitalization.lowerCase;
+              });
+            }
+          },
           borderRadius: BorderRadius.circular(widget.keyBorderRadius),
-        ),
-        constraints: const BoxConstraints(
-          minWidth: 30,
-          minHeight: 40,
-        ),
-        margin: const EdgeInsets.all(3.5),
-        child: Center(
-          child: Text(
-            capitalization == Capitalization.lowerCase
-                ? kKey.toLowerCase()
-                : kKey,
-            style: widget.alphaNumericKeyTextStyle ??
-                const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: widget.alphanumericKeyColor,
+              borderRadius: BorderRadius.circular(widget.keyBorderRadius),
+            ),
+            constraints: const BoxConstraints(
+              minWidth: 30,
+              minHeight: 40,
+            ),
+            margin: const EdgeInsets.all(marginKey),
+            child: Center(
+              child: Text(
+                capitalization == Capitalization.lowerCase ? kKey.toLowerCase() : kKey,
+                style: widget.alphaNumericKeyTextStyle ??
+                    const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
           ),
         ),
       ),
@@ -222,61 +232,66 @@ class _AlphanumericKeyboardState extends State<AlphanumericKeyboard> {
   }
 
   // Renders the action keys
-  Widget actionKey(SpecialKey kKey) {
-    return InkWell(
-      onTap: () {
-        if (kKey == SpecialKey.backspace) {
-          if (widget.controller.text.isNotEmpty) {
-            widget.controller.text = widget.controller.text
-                .substring(0, widget.controller.text.length - 1);
-          }
-        } else if (kKey == SpecialKey.space) {
-          widget.controller.text += ' ';
-        } else if (kKey == SpecialKey.enter) {
-          if (widget.onEnterTapped != null) {
-            widget.onEnterTapped!();
-          } else {
-            widget.controller.text += '\n';
-          }
-        } else if (kKey == SpecialKey.capsLock) {
-          setState(() {
-            switch (capitalization) {
-              case Capitalization.lowerCase:
-                capitalization = Capitalization.onlyFirstLetter;
-                break;
-              case Capitalization.onlyFirstLetter:
-                capitalization = Capitalization.upperCase;
-                break;
-              case Capitalization.upperCase:
-                capitalization = Capitalization.lowerCase;
-                break;
+  Widget actionKey(SpecialKey kKey, int flex) {
+    return Flexible(
+      flex: flex,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            if (kKey == SpecialKey.backspace) {
+              if (widget.controller.text.isNotEmpty) {
+                widget.controller.text = widget.controller.text.substring(0, widget.controller.text.length - 1);
+              }
+            } else if (kKey == SpecialKey.space) {
+              widget.controller.text += ' ';
+            } else if (kKey == SpecialKey.enter) {
+              if (widget.onEnterTapped != null) {
+                widget.onEnterTapped!();
+              } else {
+                widget.controller.text += '\n';
+              }
+            } else if (kKey == SpecialKey.capsLock) {
+              setState(() {
+                switch (capitalization) {
+                  case Capitalization.lowerCase:
+                    capitalization = Capitalization.onlyFirstLetter;
+                    break;
+                  case Capitalization.onlyFirstLetter:
+                    capitalization = Capitalization.upperCase;
+                    break;
+                  case Capitalization.upperCase:
+                    capitalization = Capitalization.lowerCase;
+                    break;
+                }
+              });
+            } else if (kKey == SpecialKey.symbols) {
+              setState(() {
+                if (keyboardType == KeyBoardType.alphanumeric) {
+                  keyboardType = KeyBoardType.symbols;
+                } else {
+                  keyboardType = KeyBoardType.alphanumeric;
+                }
+              });
             }
-          });
-        } else if (kKey == SpecialKey.symbols) {
-          setState(() {
-            if (keyboardType == KeyBoardType.alphanumeric) {
-              keyboardType = KeyBoardType.symbols;
-            } else {
-              keyboardType = KeyBoardType.alphanumeric;
-            }
-          });
-        }
-      },
-      borderRadius: BorderRadius.circular(widget.keyBorderRadius),
-      child: Container(
-        decoration: BoxDecoration(
-          color: widget.actionKeyColor,
+          },
           borderRadius: BorderRadius.circular(widget.keyBorderRadius),
-        ),
-        constraints: const BoxConstraints(
-          minWidth: 30,
-          minHeight: 40,
-        ),
-        margin: const EdgeInsets.all(3.5),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: getActionKeyIcon(kKey),
+          child: Container(
+            decoration: BoxDecoration(
+              color: widget.actionKeyColor,
+              borderRadius: BorderRadius.circular(widget.keyBorderRadius),
+            ),
+            constraints: const BoxConstraints(
+              minWidth: 30,
+              minHeight: 40,
+            ),
+            margin: const EdgeInsets.all(marginKey),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: getActionKeyIcon(kKey),
+              ),
+            ),
           ),
         ),
       ),
@@ -294,18 +309,84 @@ class _AlphanumericKeyboardState extends State<AlphanumericKeyboard> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           keyboardRow(KeyRows.numbersRow),
-          keyboardRow(keyboardType == KeyBoardType.alphanumeric
-              ? KeyRows.alphabetsTopRow
-              : KeyRows.symbolsTopRow),
-          keyboardRow(keyboardType == KeyBoardType.alphanumeric
-              ? KeyRows.alphabetsMiddleRow
-              : KeyRows.symbolsMiddleRow),
-          keyboardRow(keyboardType == KeyBoardType.alphanumeric
-              ? KeyRows.alphabetsBottomRow
-              : KeyRows.symbolsBottomRow),
-          keyboardRow(KeyRows.lastRow),
+          keyboardRow(keyboardType == KeyBoardType.alphanumeric ? KeyRows.alphabetsTopRow : KeyRows.symbolsTopRow),
+          keyboardRow3(
+              keyboardType == KeyBoardType.alphanumeric ? KeyRows.alphabetsMiddleRow : KeyRows.symbolsMiddleRow),
+          keyboardType == KeyBoardType.alphanumeric
+              ? keyboardRow4(KeyRows.alphabetsBottomRow)
+              : keyboardRow4Sym(KeyRows.symbolsBottomRow),
+          keyboardRow5(KeyRows.lastRow),
         ],
       ),
+    );
+  }
+
+  Widget keyboardRow3(List<String> keys) {
+    List<Widget> childrens = keys
+        .map(
+          (e) => e.length > 1
+              ? actionKey(SpecialKey.values.firstWhere((element) => element.name == e), 3)
+              : keys[0] == '1'
+                  ? numberKey(e, 2)
+                  : alphabetKey(e, 2),
+        )
+        .toList();
+    childrens.insert(0, Spacer(flex: 1));
+    childrens.add(Spacer(flex: 1));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: childrens,
+    );
+  }
+
+  Widget keyboardRow4Sym(List<String> keys) {
+    List<Widget> childrens = keys
+        .map(
+          (e) => e.length > 1
+              ? actionKey(SpecialKey.values.firstWhere((element) => element.name == e), 3)
+              : keys[0] == '1'
+                  ? numberKey(e, 2)
+                  : alphabetKey(e, 2),
+        )
+        .toList();
+    childrens.insert(0, Spacer(flex: 1));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: childrens,
+    );
+  }
+
+  Widget keyboardRow4(List<String> keys) {
+    List<Widget> childrens = keys
+        .map(
+          (e) => e.length > 1
+              ? actionKey(SpecialKey.values.firstWhere((element) => element.name == e), 3)
+              : keys[0] == '1'
+                  ? numberKey(e, 2)
+                  : alphabetKey(e, 2),
+        )
+        .toList();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: childrens,
+    );
+  }
+
+  Widget keyboardRow5(List<String> keys) {
+    List<Widget> childrens = keys
+        .map(
+          (e) => e == SpecialKey.space.name
+              ? actionKey(SpecialKey.space, 10)
+              : e.length > 1
+                  ? actionKey(SpecialKey.values.firstWhere((element) => element.name == e), 3)
+                  : keys[0] == '1'
+                      ? numberKey(e, 2)
+                      : alphabetKey(e, 2),
+        )
+        .toList();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: childrens,
     );
   }
 }
